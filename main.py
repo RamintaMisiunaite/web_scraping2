783e41a9dfafdf91f2155527c3419ea18f66d5d6
RamintaMisTwoday <139208203+RamintaMisTwoday@users.noreply.github.com>
2024-01-30 14:43:24 +0200

    added functionality so the headlines could be filtered by user's input word

--- main.py
+++ main.py
@@ -15,19 +15,36 @@ html_text = html_url.text
 soup = BeautifulSoup(html_text, 'lxml')
 
 # select only headline divs
-headline = soup.find('div', class_='news__content')
+headlines = soup.find_all('div', class_='news__content')
 
-# get the title of the headline
-headline_title = headline.find('h3').text
 
-# get the time the headline was entered
-headline_time = headline.find('span', class_='info-block__time-before')['title']
 
-# get the theme the headline is posted under
-headline_theme = headline.find('a', class_='info-block__link').text
+print('koks zodis turi but antrasteje?')
+word =  input('>')
+print(f"filtruojamos naujienos su zodziu {word}")
 
-print(f'''
-      Pavadinimas:  {headline_title},
-      Laikas: {headline_time},
-      Tema: {headline_theme}
-      ''')
+
+for headline in headlines: 
+    # get the title of the headline
+    headline_title = headline.find('h3').text
+
+    # get the time the headline was entered
+    try:
+       headline_time = headline.find('span', class_='info-block__text').text
+    except AttributeError:
+        continue       
+    
+    # get the theme the headline is posted under
+    headline_theme = headline.find('a', class_='info-block__link').text
+
+    if word in headline_title:
+        print(f"Pavadinimas:  {headline_title}")
+        print(f"Laikas:  {headline_time}")
+        print(f"Tema:  {headline_theme}")
+        print('')
+    
+    
+
+
+
+
