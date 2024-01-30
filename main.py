9fc68f52dd363231bd557a0232201b9a94756e36
RamintaMisTwoday <139208203+RamintaMisTwoday@users.noreply.github.com>
2024-01-30 13:32:29 +0200

    a single headline scrape from lrt.lt added comments for clarification

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 from bs4 import BeautifulSoup
 import requests
 
+# This application will scrape the data of the headline from lrt.lt
 
+# connect to lrt.lt
 html_url = requests.get('https://www.lrt.lt/')
 
-# Make the guessed encoding official
+# update the encoding so lithuanian letters would be recognized
 html_url.encoding = html_url.apparent_encoding
 
+# get the html text from website
 html_text = html_url.text
 
 soup = BeautifulSoup(html_text, 'lxml')
 
+# select only headline divs
 headline = soup.find('div', class_='news__content')
 
+# get the title of the headline
 headline_title = headline.find('h3').text
 
+# get the time the headline was entered
 headline_time = headline.find('span', class_='info-block__time-before')['title']
 
+# get the theme the headline is posted under
 headline_theme = headline.find('a', class_='info-block__link').text
 
 print(f'''
